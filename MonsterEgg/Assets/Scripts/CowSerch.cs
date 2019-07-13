5cd9269fb6bb4948da6909a6821c5db3378225cb
鍵和田悠太 <unconfigured@null.spigotmc.org>
2019-07-14 05:51:05 +0900

    ダッシュ中のプレイヤーが接近すると牛が全力で逃げる

---
+++ MonsterEgg/Assets/Scripts/CowSerch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowSerch : MonoBehaviour
+{
+    public GameObject Body;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = Body.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            CharacterCtrl cc = other.GetComponent<CharacterCtrl>();
+            if(cc.isDush == true)
+            {
+                Body.GetComponent<Cow>().IsEscape = true;
+                rb.AddForce(-(other.transform.position - transform.position)*10);
+                Body.transform.LookAt(-(other.transform.position - transform.position));
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Body.GetComponent<Cow>().IsEscape = false;
+        }
+    }
+
+}
