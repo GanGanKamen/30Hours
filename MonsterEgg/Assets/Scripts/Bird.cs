3b25f0fb0a68c5e0221bbec098a1ea930782288f
鍵和田悠太 <unconfigured@null.spigotmc.org>
2019-07-14 02:42:58 +0900

    鳥と、鳥をスポーンさせるためのプレハブ「Spawner」を実装、Spawnerはシーンの適当な位置に配置してください

---
+++ MonsterEgg/Assets/Scripts/Bird.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public int B_type;//鳥の初期地点　0=左 1=右　2=上　3=下
+    private Vector3 vec,pos;
+    public float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        B_type = Random.Range(0, 4);
+        rb = GetComponent<Rigidbody>();
+        if (B_type == 0)
+        {
+            pos = new Vector3(-50, 0, Random.Range(-20, 20));
+            vec = new Vector3(speed, 0, Random.Range(-speed+0.2f, speed-0.2f));
+            transform.position = pos;
+            transform.LookAt(transform.position + vec);
+
+        }
+        else if(B_type == 1)
+        {
+            pos = new Vector3(50, 0, Random.Range(-20, 20));
+            vec = new Vector3(-speed, 0, Random.Range(-speed+0.2f, speed-0.2f));
+            transform.position = pos;
+            transform.LookAt(transform.position + vec);
+        }
+        else if(B_type == 2)
+        {
+            pos = new Vector3(Random.Range(-40, 40),0,30);
+            vec = new Vector3(Random.Range(-speed+ 0.2f, speed- 0.2f),0,-1);
+            transform.position = pos;
+            transform.LookAt(transform.position + vec);
+        }
+        else if(B_type == 3)
+        {
+            pos = new Vector3(Random.Range(-40, 40), 0, -30);
+            vec = new Vector3(Random.Range(-speed+ 0.2f, speed- 0.2f), 0, 1);
+            transform.position = pos;
+            transform.LookAt(transform.position + vec);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = vec;   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.StartsWith("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ MonsterEgg/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bird;
+    public float SummonInterval;//召喚待機時間
+    float time;
+    public int MaxSimultaneousNum;//同時召喚数
+    int Num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > SummonInterval)
+        {
+            Num = Random.Range(1, MaxSimultaneousNum + 1);
+            SummonBird(Num);
+        }
+    }
+
+    void SummonBird(int x)
+    {
+        for(int i = 0; i<x; i++)
+        {
+            Instantiate(bird, new Vector3(0, -15, 0), Quaternion.identity);
+        }
+        time = 0;
+    }
+}
