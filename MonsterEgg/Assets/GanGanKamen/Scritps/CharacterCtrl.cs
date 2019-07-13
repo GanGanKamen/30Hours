c142449551fddc6c8fff11716b9843e12ea0e4e5
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-07-13 14:54:05 +0900

    移動操作。コントローラ分け

---
+++ MonsterEgg/Assets/GanGanKamen/Scritps/CharacterCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCtrl : MonoBehaviour
+{
+    public float speed;
+    public GameObject body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CharacterMove(Vector3 direction)
+    {
+        transform.Translate(direction * Time.deltaTime * speed);
+        body.transform.localRotation = Quaternion.LookRotation(direction);
+    }
+}
---
+++ MonsterEgg/Assets/GanGanKamen/Scritps/PlayerCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public int dualshock4Num;
+    private CharacterCtrl character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterCtrl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharaMove();
+    }
+
+    private void KeyCtrl()
+    {
+        
+    }
+
+    private void CharaMove()
+    {
+        if(Dualshock4.LeftStick(dualshock4Num).magnitude != 0)
+        {
+            var direction = new Vector3(Dualshock4.LeftStick(dualshock4Num).x,0, Dualshock4.LeftStick(dualshock4Num).y);
+                //Dualshock4.LeftStick(dualshock4Num);
+            character.CharacterMove(direction);
+        }
+    }
+}
