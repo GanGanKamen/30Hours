c622085d973929e79367d90434e768ff995b65ed
鍵和田悠太 <unconfigured@null.spigotmc.org>
2019-07-13 20:49:42 +0900

    お魚更新

--- MonsterEgg/Assets/Scripts/Fish.cs
+++ MonsterEgg/Assets/Scripts/Fish.cs
@@ -7,10 +7,11 @@ public class Fish : MonoBehaviour
     private float time;
     public float MoveInterval, MoveSize,MoveTime;
     private Vector3 vec;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -29,9 +30,10 @@ public class Fish : MonoBehaviour
         vec = new Vector3(Random.Range(-MoveSize, MoveSize), 0, Random.Range(-MoveSize, MoveSize));
         transform.LookAt(transform.position +vec);
         time = 0;
+        rb.AddForce(vec * 100);
         //transform.Translate(vec);
         //transform.position = Vector3.Lerp(transform.position, transform.position+vec, Time.deltaTime * 2);
-        StartCoroutine(move(vec));
+        //StartCoroutine(move(vec));
         
 
     }
