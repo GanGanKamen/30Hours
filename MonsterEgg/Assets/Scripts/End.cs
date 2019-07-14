5424c5f0bd7736db8bce34b516db4ff16f6aef95
鍵和田悠太 <unconfigured@null.spigotmc.org>
2019-07-14 10:47:44 +0900

    エンドのシーンを追加

---
+++ MonsterEgg/Assets/Scripts/End.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class End : MonoBehaviour
+{
+    bool waited;
+    public GameObject Tex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waited = false;
+        StartCoroutine(wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waited == true)
+        {
+
+            if (Dualshock4.CircleDown(0))
+            {
+                Fader.switchScene("Title");
+            }
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2f);
+        Tex.SetActive(true);
+        waited = true;
+    }
+}
